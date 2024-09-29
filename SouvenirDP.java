c1866de9bab3a7581d8f3b9a1652b7a9963a62ec
William Alexander <williamalexander@Williams-MacBook-Air.local>
2024-09-29 10:03:00 +0700

    new DP progress

--- SouvenirDP.java
+++ SouvenirDP.java
@@ -24,7 +24,7 @@ public class SouvenirDP {
 
     public static void insertDP(int lastInserted, int budget, int[] hargaSuvenir, int[] kebahagiaanSuvenir) {
         
-        int N = hargaSuvenir.length;
+        long N = hargaSuvenir.length;
 
         if (lastInserted == 0) {
             ArrayList<ArrayList<ArrayList<Long>>> row = new ArrayList<>();
@@ -42,7 +42,7 @@ public class SouvenirDP {
 
         for (long uang=lastInserted; uang<=budget; uang++) {
 
-            ArrayList<ArrayList<ArrayList<Long>>> row = new ArrayList<ArrayList<ArrayList<Long>>>(N);
+            ArrayList<ArrayList<ArrayList<Long>>> row = new ArrayList<ArrayList<ArrayList<Long>>>();
 
             System.out.println();
             System.out.print("Uang: ");
@@ -81,12 +81,12 @@ public class SouvenirDP {
                 ArrayList<Long> fourthIndices = new ArrayList<>();
 
 
-                if (j<(N-1)) {  
-                    firstCandidate = row.get(0);
+                if (j<(N-1)) {  // Tidak membeli suvenir j
+                    firstCandidate = row.get(0);  // Samping
                     firstKebahagiaan = firstCandidate.get(0).get(0);
                     firstIndices = firstCandidate.get(1);   
-                    // System.out.print("First candidate: ");
-                    // System.out.println(firstCandidate);
+                    System.out.print("First candidate: ");
+                    System.out.println(firstCandidate);
                     // System.out.print("First kebahagiaan: ");
                     // System.out.println(firstKebahagiaan);
                     // System.out.print("First Indices: ");             
@@ -94,10 +94,9 @@ public class SouvenirDP {
                     
                 }
 
-
-                secondCandidate = dp.get((int) uang - 1).get((int) j);
-                // System.out.print("Second candidate: ");
-                // System.out.println(secondCandidate);
+                secondCandidate = dp.get((int) uang - 1).get((int) j);  // Atas (budget sebelumnya), tidak membeli suvenir j
+                System.out.print("Second candidate: ");
+                System.out.println(secondCandidate);
                 secondKebahagiaan = secondCandidate.get(0).get(0);
                 // System.out.print("Second kebahagiaan: ");
                 // System.out.println(secondKebahagiaan);
@@ -106,42 +105,90 @@ public class SouvenirDP {
                 // System.out.println(secondIndices);                        
                 
 
-                if (uang >= harga) {
-                    int[] indexTakeThird = candidate(uang, harga, j, j, N);
-                    if (indexTakeThird == null) {
-                        thirdCandidate = null;
-                    } else {
-                        thirdCandidate = dp.get((int) uang - harga).get(indexTakeThird[0]);
-                        // System.out.print("Third candidate: ");
+                if (uang >= harga) {  // Membeli suvenir j
+                    
+                    if (j < N-2) {
+            
+                        thirdCandidate = dp.get((int) uang - harga).get((int) j+2);
+                        System.out.print("Third candidate(h[j] + dp[j+2]): ");
                         System.out.println(thirdCandidate);
                         thirdKebahagiaan = thirdCandidate.get(0).get(0) + kebahagiaan;
                         // System.out.print("Third kebahagiaan: ");
                         // System.out.println(thirdKebahagiaan);
                         thirdIndices.add(j);
-                        thirdIndices.addAll(thirdCandidate.get(indexTakeThird[1]));  
+                        thirdIndices.addAll(thirdCandidate.get(1));  
                         // System.out.print("Third Indices: ");             
                         // System.out.println(thirdIndices);    
+                        
+                        int hargaSebelum = hargaSuvenir[(int) j+1];  // harga suvenir sebelum (j+1)
+                        int kebahagiaanSebelum = kebahagiaanSuvenir[(int) j+1];  // kebahagiaan suvenir sebelum (j+1)
+
+                        if (j < (N-3) && uang >= (harga+hargaSebelum)) {
+                            
+                            fourthCandidate = dp.get((int) uang - harga - hargaSebelum).get((int) j+3);  // harga + hargaSebelum(j+1) + dp longkap 1(j+3) (agar tidak consecutive 3)
+                            fourthKebahagiaan = fourthCandidate.get(0).get(0) + kebahagiaan + kebahagiaanSebelum;
+                            System.out.printf("Fourth candidate(h[j](%d) + h[j+1](%d) + dp[j+3])(%d): ", kebahagiaan, kebahagiaanSebelum, fourthKebahagiaan);
+                            System.out.println(fourthCandidate);
+                            fourthIndices.add(j);
+                            fourthIndices.add(j+1);
+                            fourthIndices.addAll(fourthCandidate.get(1));                    
+                        } else if ((j == (N-3))) {
+
+                            if ((uang >= (harga + hargaSuvenir[(int) j+1])) && uang >= (harga + hargaSuvenir[(int) j+2])) {
+                                System.out.print("kebahagiaanSuvenir[j+1]: ");
+                                System.out.println(kebahagiaanSuvenir[(int) j+1]);
+
+                                System.out.print("kebahagiaanSuvenir[j+2]: ");
+                                System.out.println(kebahagiaanSuvenir[(int) j+2]);
+
+                                if (kebahagiaanSuvenir[(int) j+1] >= kebahagiaanSuvenir[(int) j+2]) {
+                                    fourthCandidate = row.get(0);
+                                    System.out.print("Fourth candidate(h[j]: ");
+                                    System.out.println(fourthCandidate);
+                                    fourthKebahagiaan = kebahagiaanSuvenir[(int) j+1] + kebahagiaan;
+                                    fourthIndices.add(j);
+                                    fourthIndices.add(j+1);
+                                } else {
+                                    fourthCandidate = row.get(1);
+                                    System.out.print("Fourth candidate(h[j]: ");
+                                    System.out.println(fourthCandidate);
+                                    fourthKebahagiaan = kebahagiaanSuvenir[(int) j+2] + kebahagiaan;
+                                    fourthIndices.add(j);
+                                    fourthIndices.add(j+2);
+                                }
+                            } 
+
+
+
+                        }
+                    } else if (j >= (N-2)) {
+
+                        if (j == (N-2)) {
+                            thirdCandidate = dp.get((int) uang-harga).get((int) j+1);
+                            System.out.print("Third candidate: ");
+                            System.out.println(thirdCandidate);
+                            thirdKebahagiaan = thirdCandidate.get(0).get(0) + kebahagiaan;
+                            thirdIndices.add(j);
+                            thirdIndices.addAll(thirdCandidate.get(1));
+                        } else if ((j == (N-1)) && harga == uang) {
+                            thirdCandidate = dp.get((int) uang-harga).get((int) j);
+                            System.out.print("Third candidate: ");
+                            System.out.println(thirdCandidate);
+                            thirdKebahagiaan = thirdCandidate.get(0).get(0) + kebahagiaan;
+                            thirdIndices.add(j);
+                            thirdIndices.addAll(thirdCandidate.get(1));
+                        }
+
+
                     }
-                    
-                    int[] indexTakeFourth = candidate(uang, harga, j, j+1, N);
-                    if (indexTakeFourth == null) fourthCandidate = null;
-                    else {
-                        fourthCandidate = dp.get((int) uang - harga).get(indexTakeThird[0]);
-                        fourthKebahagiaan = fourthCandidate.get(0).get(0) + kebahagiaan;
-                        System.out.print("Fourth Kebahagiaan: ");
-                        System.out.println(fourthKebahagiaan);
-                        fourthIndices.add(j);
-                        fourthIndices.addAll(fourthCandidate.get(indexTakeFourth[1]));
-                    }                        
-                    
 
                 }
 
                 long maxKebahagiaan = 0;
-                System.out.print(firstCandidate);
-                System.out.print(secondCandidate);
-                System.out.print(thirdCandidate);
-                System.out.println(fourthCandidate);
+                // System.out.print(firstCandidate);
+                // System.out.print(secondCandidate);
+                // System.out.print(thirdCandidate);
+                // System.out.println(fourthCandidate);
 
                 if (firstCandidate != null) {
                     if (firstKebahagiaan > maxKebahagiaan) {
@@ -170,6 +217,7 @@ public class SouvenirDP {
                 if (fourthCandidate != null) {
                     if (fourthKebahagiaan > maxKebahagiaan) {
                         maxKebahagiaan = fourthKebahagiaan;
+                        resIndices.clear();
                         resIndices.add(fourthIndices);
                     }
                 }
